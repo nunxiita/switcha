b07217965aa098bad5bc4ce1080c851f32eb2173
José Manuel Suárez Tocino <nunxiita@outlook.es>
2021-10-15 20:19:13 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES']});
+
+
+
+client.login("ODk4NTcwMzQ5MjI1MDUwMTEy.YWmI2Q.r1qWOsZ_fxaaGfWIrgm6SCJP5KI");
